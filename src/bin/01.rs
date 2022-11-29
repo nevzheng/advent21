143808ded26fbabe03759e69c4e5cb437e854148
Nevin Zheng <nlz@google.com>
2022-11-29 05:15:28 +0000

    solve day1, day2

---
+++ src/bin/01.rs
@@ -0,0 +1,58 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let depths: Vec<u32> = input
+        .lines()
+        .filter_map(|x| x.parse::<u32>().ok())
+        .collect();
+
+    let mut n_rises = 0;
+
+    for w in depths.windows(2) {
+        if w[0] < w[1] {
+            n_rises += 1;
+        }
+    }
+
+    Some(n_rises)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let depths: Vec<u32> = input
+        .lines()
+        .filter_map(|x| x.parse::<u32>().ok())
+        .collect();
+
+    let mut last_sum = u32::MAX;
+    let mut n_greater = 0;
+
+    for w in depths.windows(3) {
+        if last_sum < w.iter().sum() {
+            n_greater += 1;
+        }
+        last_sum = w.iter().sum();
+    }
+
+    Some(n_greater)
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 1);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,76 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut x = 0;
+    let mut y = 0;
+
+    let commands: Vec<(&str, u32)> = input
+        .lines()
+        .filter_map(|l| l.split_once(' '))
+        .filter_map(|(x, y_s)| {
+            let y_i = y_s.parse::<u32>().ok()?;
+            Some((x, y_i))
+        })
+        .collect();
+
+    for (direction, count) in commands {
+        match direction {
+            "forward" => x += count,
+            "up" => y -= count,
+            "down" => y += count,
+            _ => panic!(),
+        }
+    }
+
+    Some(x * y)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+
+    let commands: Vec<(&str, u32)> = input
+        .lines()
+        .filter_map(|l| l.split_once(' '))
+        .filter_map(|(x, y_s)| {
+            let y_i = y_s.parse::<u32>().ok()?;
+            Some((x, y_i))
+        })
+        .collect();
+
+    for (direction, count) in commands {
+        match direction {
+            "forward" => {
+                x += count;
+                y += aim * count;
+            }
+            "up" => aim -= count,
+            "down" => aim += count,
+            _ => panic!(),
+        }
+    }
+
+    Some(x * y)
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 2);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 2);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 2);
+        assert_eq!(part_two(&input), None);
+    }
+}
